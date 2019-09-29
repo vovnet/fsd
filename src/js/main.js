3495382efb473936571c7fe32c0f96dc25114810
vovnet <vovnet@yandex.ru>
2019-09-29 10:16:09 +0300

    added date dropdown

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,33 @@
 import expander from '../blocks/expander/expander';
 import conf from '../blocks/configuration-dropdown/configuration-dropdown';
+import 'jquery-ui/ui/widgets/datepicker';
+
+$.datepicker.regional['ru'] = {
+    closeText: 'Закрыть',
+    prevText: 'Пред',
+    nextText: 'След',
+    currentText: 'Сегодня',
+    monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь',
+    'Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+    monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн',
+    'Июл','Авг','Сен','Окт','Ноя','Дек'],
+    dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'],
+    dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'],
+    dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+    weekHeader: 'Нед',
+    dateFormat: 'dd.mm.yy',
+    firstDay: 1,
+    isRTL: false,
+    showMonthAfterYear: false,
+    yearSuffix: ''};
+$.datepicker.setDefaults($.datepicker.regional['ru']);  
+
+let options = {
+    showOtherMonths: true,
+    showButtonPanel: true,
+    prevText: '<i class="material-icons">arrow_back</i>',
+    nextText: '<i class="material-icons">arrow_forward</i>'
+};
+
+$('#first_date').datepicker(options);
+$('#second_date').datepicker(options);
