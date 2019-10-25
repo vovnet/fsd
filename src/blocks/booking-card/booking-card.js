a7690612b1a246005d8427c7f7331b1d1ded8c33
vovnet <vovnet@yandex.ru>
2019-10-25 16:15:30 +0300

    added hero-galery

--- src/blocks/booking-card/booking-card.js
+++ src/blocks/booking-card/booking-card.js
@@ -12,3 +12,15 @@ Calendar({
     container: $('#booking_calendar'),
     inputs: $('#first_booking_date,#second_booking_date')
 })
+
+$('#booking_dropdown').dropdown({
+    title: 'гости',
+    show: false,
+    placeholder: '3 гостя',
+    buttons: false,
+    mapNames: ['гость', 'гостя', 'гостей'],
+    elements: [
+        { title: 'взрослых', value: 3 },
+        { title: 'детей', value: 0 }
+    ]
+});
--- src/pages/room-details/room-details.js
+++ src/pages/room-details/room-details.js
@@ -4,3 +4,4 @@ import '../../blocks/booking-card/booking-card';
 import '../../blocks/comment/comment';
 import '../../blocks/additions/additions';
 import '../../blocks/bullet/bullet';
+import '../../blocks/hero-galery/hero-galery';
