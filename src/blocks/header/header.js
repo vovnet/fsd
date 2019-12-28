df0c5f17136530085953a6bb77aeb312baff53a6
vovnet <vovnet@yandex.ru>
2019-12-28 18:48:24 +0300

    delete code from header.js

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -2,9 +2,4 @@ import './header.scss';
 import '../menu/menu';
 import '../rounded-button/rounded-button';
 import '../button/button';
-import '../menu-toggle/menu-toggle';
 import '../icon-button/icon-button';
-
-$('#menu_toggle').click( e => {
-    $('.menu').toggleClass('menu--open');
-});
--- src/blocks/menu/menu.js
+++ src/blocks/menu/menu.js
@@ -1,19 +1,21 @@
 import $ from 'jquery';
 import './menu.scss';
 
-$('.menu__menu-toggle').click( e => {
-    $(e.currentTarget).toggleClass('menu--open');
-})
+$(document).ready(function() {
+    $('.header__menu-toggle').click(e => {
+        $('.menu').toggleClass('menu--open');
+    });
 
-$('.menu__submenu').click( (e) => {
-    $(e.currentTarget).children('.submenu').toggleClass('submenu--open');
-})
+    $('.menu__submenu').on('click', (e) => {
+        $(e.currentTarget).children('.submenu').toggleClass('submenu--open');
+    });
 
-$(document.body).not('.menu__submenu').click(e => {
-    if (!$(e.target).is('.menu__submenu') ) {
-        let menu = $('.submenu');
-        if (menu.hasClass('submenu--open')) {
-            menu.removeClass('submenu--open');
+    $(document.body).not('.menu__submenu').click(e => {
+        if (!$(e.target).is('.menu__submenu')) {
+            let menu = $('.submenu');
+            if (menu.hasClass('submenu--open')) {
+                menu.removeClass('submenu--open');
+            }
         }
-    }
+    });
 });
