c3cbe2dfca38cf14eaad288f40c95b8ea4ad3c5b
vovnet <vovnet@yandex.ru>
2020-01-28 18:08:34 +0300

    #7 refactor datepicker

---
+++ src/blocks/calendar/DatePicker.js
@@ -0,0 +1,72 @@
+import 'air-datepicker/dist/js/datepicker';
+
+class DatePicker {
+    constructor(element) {
+        this.element = element;
+
+        this.makeDatepicker();
+        this.addHandlers();        
+    }
+
+    makeDatepicker() {
+        this.inputs = $(this.element).parent().find('input').not('.js-hidden');
+
+        const options = {
+            range: true,
+            multipleDates: true,
+            onSelect: this.onSelect.bind(this),
+            prevHtml: '<i class="material-icons">arrow_back</i>',
+            nextHtml: '<i class="material-icons">arrow_forward</i>',
+            navTitles: {
+                days: 'MM yyyy'
+            },
+        };
+
+        this.datepicker = $(this.element).datepicker(options).data('datepicker');
+
+        this.clearButton = $('<span class="simple-button simple-button--disabled">Очистить</span>');
+        this.applyButton = $('<span class="simple-button">Применить</span></div>');
+        let btnContainer = $('<div class="datepicker--btn-container">')
+            .append(this.clearButton)
+            .append(this.applyButton);
+
+        $(this.datepicker.$datepicker).append(btnContainer);
+    }
+
+    addHandlers() {
+        this.clearButton.on('click', this.onClickClear.bind(this));
+        this.applyButton.on('click', this.onClickApply.bind(this));
+        this.inputs.on('click', this.onClickInput.bind(this));
+    }
+
+    clearInputs() {
+        this.inputs.each(function () {
+            this.value = '';
+        });
+    }
+
+    onSelect(formattedDate, date, inst) {
+        this.dates = date;
+    }
+
+    onClickClear() {
+        this.clearInputs();
+        this.datepicker.clear();
+    }
+
+    onClickInput() {
+        this.datepicker.show();
+    }
+
+    onClickApply() {
+        this.inputs.each((i, el) => {
+            $(el).val(this.dates[i].toLocaleDateString());
+        });
+
+        this.datepicker.hide();
+    }
+
+    
+}
+
+export { DatePicker }
